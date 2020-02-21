c51abde340856e1007168f6b4a6cdd38c1869a8b
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-02-21 13:45:33 +0000

    adds add to list functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const inputForm = document.querySelector('#input-form');
-  form.addEventListener('click', handleInputForm);
+  const inputForm = find('#input-form');
+  inputForm.addEventListener('submit', handleInputForm);
+
+  const button = find('#button');
+  button.addEventListener('click', handleButtonClick);
 });
 
-const handleInputForm = function () => {
+const handleInputForm = function (event) {
   event.preventDefault();
-  const bikeList = reduce.querySelector('bike-list');
-  const newBike = document.createNewElement('li');
+  const bikeList = find('#bike-list');
+  const newBike = document.createElement('li');
 
+  const newMake = document.createElement('h2');
+  newMake.textContent = `${event.target.make.value}`;
+  newBike.appendChild(newMake);
+
+  const newModel = document.createElement('h3');
+  newModel.textContent = `${event.target.model.value}`;
+  newBike.appendChild(newModel);
+
+  const newCategory = document.createElement('p');
+  newCategory.textContent = `${event.target.category.value}`;
+  newBike.appendChild(newCategory);
+
+
+  bikeList.appendChild(newBike);
 };
+
+const handleButtonClick = function () {
+  const bikeList = find('#bike-list')
+};
+
+
+const find = function (string) {
+  return document.querySelector(`${string}`);
+}
