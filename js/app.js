f21f3f1f2ff05cb1b2f3930fd4127343ebb2b92b
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-02-21 13:08:51 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const inputForm = document.querySelector('#input-form');
+  form.addEventListener('click', handleInputForm);
+});
+
+const handleInputForm = function () => {
+  event.preventDefault();
+  const bikeList = reduce.querySelector('bike-list');
+  const newBike = document.createNewElement('li');
+
+};
