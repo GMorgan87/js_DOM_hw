927f8ecd88a1e5f90315f7f3dadd8b0b1c87b793
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-02-22 15:26:55 +0000

    refactors functions

--- js/app.js
+++ js/app.js
@@ -9,22 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleInputForm = function (event) {
   event.preventDefault();
   const bikeList = find('#bike-list');
-  const newBike = document.createElement('li');
-
-  const newMake = document.createElement('h2');
-  newMake.textContent = `${event.target.make.value}`;
-  newBike.appendChild(newMake);
-
-  const newModel = document.createElement('h3');
-  newModel.textContent = `${event.target.model.value}`;
-  newBike.appendChild(newModel);
-
-  const newCategory = document.createElement('p');
-  newCategory.textContent = `${event.target.category.value}`;
-  newBike.appendChild(newCategory);
-
-
+  const newBike = createBikeItem(event.target)
   bikeList.appendChild(newBike);
+  this.reset();
+};
+
+const addListItem = function (id, tag, list) {
+  const newItem = document.createElement(tag);
+  newItem.textContent = find(`#${id}`).value;
+  list.appendChild(newItem);
+};
+
+const createBikeItem = function (form) {
+  const bike = document.createElement('li');
+  addListItem('make', 'h1', bike);
+  addListItem('model', 'h2', bike);
+  addListItem('category', 'h2', bike);
+  return bike;
 };
 
 const handleButtonClick = function () {
@@ -34,5 +35,5 @@ const handleButtonClick = function () {
 
 
 const find = function (string) {
-  return document.querySelector(`${string}`);
+  return document.querySelector(string);
 }
